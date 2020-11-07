101b78ec99836b8cb4c1df1e412e8c26a5d18fb0
Alex Shevelev <alex.s@softmine.ru>
2020-11-08 01:42:22 +0300

    Lesson 4 homework: Activities data exchange

--- app/src/main/java/ru/softmine/weatherapp/BundleKeys.java
+++ app/src/main/java/ru/softmine/weatherapp/BundleKeys.java
@@ -3,4 +3,8 @@ package ru.softmine.weatherapp;
 public class BundleKeys {
     public static final String FORECAST = "forecastKey";
     public static final String TEMPERATURE = "temperatureKey";
+
+    public static final String CITYNAME = "cityName";
+    public static final String TEMPUNITS = "temperatureUnits";
+    public static final String SPEEDUNITS = "speedUnits";
 }
